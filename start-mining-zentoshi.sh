1343980267a4c05884b858cd0ddb92975dc4332e
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-18 11:09:47 +0700

    Add files via upload

---
+++ start-mining-zentoshi.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#This is an example you can edit and use
+#There are numerous parameters you can set, please check Help and Examples folder
+
+./SRBMiner-MULTI --disable-gpu --algorithm balloon_zentoshi --pool pool.zentoshi.com:8444 --wallet zentoshi-wallet-here --password c=ZEN
