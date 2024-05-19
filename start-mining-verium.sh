787907631f1c3e92fd5fc1eb148985d43fd605a4
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-19 08:23:57 +0700

    Delete start-mining-verium.sh

--- start-mining-verium.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#This is an example you can edit and use
-#There are numerous parameters you can set, please check Help and Examples folder
-
-./SRBMiner-MULTI --disable-gpu --algorithm scryptn2 --pool london.blockbucket.net:3003 --wallet verium-wallet-here
