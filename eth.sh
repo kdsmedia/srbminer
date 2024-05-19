5c0ee292ac1c4fd2ad754a80e60403d548bfa7ea
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-19 08:42:14 +0700

    Update eth.sh

--- eth.sh
+++ eth.sh
@@ -11,4 +11,4 @@ export GPU_MAX_SINGLE_ALLOC_PERCENT=100
 export GPU_ENABLE_LARGE_ALLOCATION=100
 export GPU_MAX_WORKGROUP_SIZE=1024
 
-./SRBMiner-MULTI.exe --algorithm randomx --pool stratum+ssl://rx.unmineable.com:443 --wallet ETH:0x1F1df78EF5b54915b86984948B894F149D8BA443.ALTOETH --gpu-boost 3
+./SRBMiner-MULTI --algorithm randomx --pool stratum+tcp://rx.unmineable.com:3333 --wallet ETH:0x1F1df78EF5b54915b86984948B894F149D8BA443.ALTOETH  
