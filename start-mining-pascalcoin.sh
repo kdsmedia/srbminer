b4869964c070b66d2324242123cfe37a3226faec
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-19 08:24:23 +0700

    Delete start-mining-pascalcoin.sh

--- start-mining-pascalcoin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#This is an example you can edit and use
-#There are numerous parameters you can set, please check Help and Examples folder
-
-./SRBMiner-MULTI --disable-gpu --algorithm randomhash2 --pool fastpool.xyz:3338 --wallet pascal-wallet-here
