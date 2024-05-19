c2fd4bb469124193af5b2daceca23ea65485e2fc
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-05-19 08:24:52 +0700

    Delete start-mining-veruscoin.sh

--- start-mining-veruscoin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#This is an example you can edit and use
-#There are numerous parameters you can set, please check Help and Examples folder
-
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.eu.mine.zergpool.com:3300 --wallet R9X4txHSAVXLqc6LNtc9NcSei7RjCJkw2A
